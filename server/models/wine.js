8e04efdf1dbdbfc4f56e97e3137a5aebc3c14d14
Ian Holmes <iqholmes@gmail.com>
2022-07-27 15:31:04 -0400

    wine by id working

--- server/models/wine.js
+++ server/models/wine.js
@@ -15,9 +15,9 @@ const wineSchema = new Schema ({
   comments: [{ type: Schema.Types.ObjectId, ref: "comment" }]
 });
 
-const Wine = mongoose.model('wine', wineSchema);
+const Wine = mongoose.model('Wine', wineSchema);
 
-// let daou1 = new Wine({image: 'https://www.wine-searcher.com/images/labels/22/86/11152286.jpg?width=466&height=400&fit=bounds', winery: 'Daou Vineyards', wine:'Patrimony Caves des Lions', region:'Adelaida District, USA', vintage: 2017, blend: 'Cabernet Franc - Cabernet Sauvignon', pairing: 'Beef, Lamb, Venison', comments: []})
+let daou1 = new Wine({image: 'https://www.wine-searcher.com/images/labels/22/86/11152286.jpg?width=466&height=400&fit=bounds', winery: 'Daou Vineyards', wine:'Patrimony Caves des Lions', region:'Adelaida District, USA', vintage: 2017, blend: 'Cabernet Franc - Cabernet Sauvignon', pairing: 'Beef, Lamb, Venison', comments: []})
 // daou1.save();
 
 // let daou2 = new Wine({image: 'https://www.wine-searcher.com/images/labels/05/72/10980572.jpg?width=466&height=400&fit=bounds', winery: 'Daou Vineyards', wine:'Eye of the Falcon', region:'Paso Robles, CA, USA', vintage: 2016, blend: 'Cabernet - Petit Verdot', pairing: 'Lamb', comments: []})
@@ -35,4 +35,4 @@ const Wine = mongoose.model('wine', wineSchema);
 // let ah2 = new Wine({image: 'https://hopefamilywines.com/wp-content/uploads/2021/12/OUTSHINERY-HopeFamily-AustinHope-CabSauv_334x1286.png', winery: 'Austin Hope', wine:'Cabernet Sauvignon', region:'Paso Robles, CA, USA', vintage: 2020, blend: 'Cabernet Sauvignon', pairing: 'Beef and Venison', comments: []})
 // ah2.save();
 
-module.exports = Wine;
+module.exports = mongoose.model('Wine', wineSchema);
--- server/routes/main.js
+++ server/routes/main.js
@@ -5,11 +5,27 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+router.param('id', (req, res, next, id) => {
+  Wine.findById(id, function(err, wine){
+    if (err) {
+      return res.status(404).send('No wine with that ID found!');
+    } else if (wine) {
+      req.wine = wine;
+      next();
+    }
+  });
+});
+
 //get all wines
 router.get("/wines", (req, res) => {
   Wine.find((error, wine) => {
     res.send(wine);
   })
 })
+
+//get specific wine by id
+router.get("/wines/:id", (req, res) => {
+  res.send(req.wine);
+});
 
 module.exports = router;
